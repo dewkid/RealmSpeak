8071fe21cc5220af0308607293889aecbf3f53b7
Simon Hunt <simon@onlab.us>
2016-03-26 11:30:52 -0700

    Initial cleanup of remaining classes in general_io (remove tabs, update copyright, ...)

--- general/utility/general_io/source/com/robin/general/io/RecordParser.java
+++ general/utility/general_io/source/com/robin/general/io/RecordParser.java
@@ -1,77 +1,87 @@
-/* 
+/*
  * RealmSpeak is the Java application for playing the board game Magic Realm.
- * Copyright (c) 2005-2015 Robin Warren
+ * Copyright (c) 2005-2016 Robin Warren
  * E-mail: robin@dewkid.com
- * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this program. If not, see
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see
  *
  * http://www.gnu.org/licenses/
  */
 package com.robin.general.io;
 
-import java.util.*;
+import java.util.StringTokenizer;
 
+/**
+ * Parses records.
+ * @deprecated not used in the codebase
+ */
+@Deprecated
 public class RecordParser {
 
-	private String[] field;
-	
-	public RecordParser(String line,String delim) {
-		line = insertSpaces(line,delim);
-		StringTokenizer tokens = new StringTokenizer(line.toString(),delim);
-		field = new String[tokens.countTokens()];
-		for (int i=0;i<field.length;i++) {
-			field[i] = tokens.nextToken();
-		}
-	}
-	
-	private static String insertSpaces(String val,String delim) {
-		if (val.startsWith(delim)) {
-			val = " "+val;
-		}
-		if (val.endsWith(delim)) {
-			val = val+" ";
-		}
-		int index;
-		while((index = val.indexOf(delim+delim))>=0) {
-			val = val.substring(0,index+1)+" "+val.substring(index+1);
-		}
-		return val;
-	}
-	
-	public int totalFields() {
-		return field.length;
-	}
-	
-	public String getField(int index) {
-		if (index>0 && index<field.length) {
-			return stripEndQuotes(field[index]);
-		}
-		return "";
-	}
-	public static String stripEndQuotes(String val) {
-		if (val!=null && val.length()>1) {
-			if (val.startsWith("\"") && val.endsWith("\"")) {
-				val = val.substring(1,val.length()-1);
-			}
-		}
-		return val;
-	}
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		for (int i=0;i<field.length;i++) {
-			if (i>0) {
-				sb.append(",");
-			}
-			sb.append(field[i]);
-		}
-		return sb.toString();
-	}
+    private String[] field;
+
+    public RecordParser(String line, String delim) {
+        line = insertSpaces(line, delim);
+        StringTokenizer tokens = new StringTokenizer(line.toString(), delim);
+        field = new String[tokens.countTokens()];
+        for (int i = 0; i < field.length; i++) {
+            field[i] = tokens.nextToken();
+        }
+    }
+
+    private static String insertSpaces(String val, String delim) {
+        if (val.startsWith(delim)) {
+            val = " " + val;
+        }
+        if (val.endsWith(delim)) {
+            val = val + " ";
+        }
+        int index;
+        while ((index = val.indexOf(delim + delim)) >= 0) {
+            val = val.substring(0, index + 1) + " " + val.substring(index + 1);
+        }
+        return val;
+    }
+
+    public int totalFields() {
+        return field.length;
+    }
+
+    public String getField(int index) {
+        if (index > 0 && index < field.length) {
+            return stripEndQuotes(field[index]);
+        }
+        return "";
+    }
+
+    public static String stripEndQuotes(String val) {
+        if (val != null && val.length() > 1) {
+            if (val.startsWith("\"") && val.endsWith("\"")) {
+                val = val.substring(1, val.length() - 1);
+            }
+        }
+        return val;
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < field.length; i++) {
+            if (i > 0) {
+                sb.append(",");
+            }
+            sb.append(field[i]);
+        }
+        return sb.toString();
+    }
 }
--- general/utility/general_io/source/com/robin/general/io/ResourceFinder.java
+++ general/utility/general_io/source/com/robin/general/io/ResourceFinder.java
@@ -1,78 +1,89 @@
-/* 
+/*
  * RealmSpeak is the Java application for playing the board game Magic Realm.
- * Copyright (c) 2005-2015 Robin Warren
+ * Copyright (c) 2005-2016 Robin Warren
  * E-mail: robin@dewkid.com
- * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this program. If not, see
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see
  *
  * http://www.gnu.org/licenses/
  */
 package com.robin.general.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
+/**
+ * Finds resources.
+ */
 public class ResourceFinder {
-	private static final ClassLoader sysLoader=ClassLoader.getSystemClassLoader();
+    private static final ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
 
-	public static InputStream getInputStream(String path) {
-		InputStream stream = null;
-		
-		File f=new File(path);
-		if (f.exists()) {
-			try {
-				return new FileInputStream(f);
-			}
-			catch(IOException ex) {
-				// ignore
-			}
-		}
-		
-		URL u=sysLoader.getResource(path);
-		if (u!=null) {
-			try {
-				stream = u.openStream();
-			}
-			catch(IOException ex) { }
-		}
-		return stream;
-	}
-	public static boolean exists(String path) {
-		File f=new File(path);
-		if (f.exists()) {
-			return true;
-		}
-		URL u=sysLoader.getResource(path);
-		if (u!=null) {
-			return true;
-		}
-		return false;
-	}
-	public static String getString(String path) {
-		try {
-			InputStream stream = getInputStream(path);
-			if (stream!=null) {
-				StringBuffer sb = new StringBuffer();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-				String line;
-				while((line=reader.readLine())!=null) {
-					sb.append(line);
-					sb.append("\n");
-				}
-				return sb.toString();
-			}
-		}
-		catch(IOException ex) {
-			// Do nothing
-		}
-		return null;
-	}
+    public static InputStream getInputStream(String path) {
+        InputStream stream = null;
+
+        File f = new File(path);
+        if (f.exists()) {
+            try {
+                return new FileInputStream(f);
+            } catch (IOException ex) {
+                // ignore
+            }
+        }
+
+        URL u = sysLoader.getResource(path);
+        if (u != null) {
+            try {
+                stream = u.openStream();
+            } catch (IOException ex) {
+            }
+        }
+        return stream;
+    }
+
+    public static boolean exists(String path) {
+        File f = new File(path);
+        if (f.exists()) {
+            return true;
+        }
+        URL u = sysLoader.getResource(path);
+        if (u != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String getString(String path) {
+        try {
+            InputStream stream = getInputStream(path);
+            if (stream != null) {
+                StringBuffer sb = new StringBuffer();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+                return sb.toString();
+            }
+        } catch (IOException ex) {
+            // Do nothing
+        }
+        return null;
+    }
 }
