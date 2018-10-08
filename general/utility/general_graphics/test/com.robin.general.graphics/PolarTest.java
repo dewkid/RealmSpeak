025cddc19c1641bb57dc13d0920c7db9e4171abf
Simon Hunt <sdhunt@gmail.com>
2018-10-08 14:21:35 -0700

    Unit tests for Polar.

--- general/utility/general_graphics/test/com.robin.general.graphics/PolarTest.java
+++ general/utility/general_graphics/test/com.robin.general.graphics/PolarTest.java
@@ -48,7 +48,115 @@ public class PolarTest extends AbstractTest {
         assertThat(polar.getLength(), is(0));
         assertThat(polar.getAngle(), is(0));
         assertThat(polar.getOrigin(), is(p(0, 0)));
+        assertThat(polar.getRect(), is(p(0, 0)));
         assertThat(polar.getX(), is(0));
         assertThat(polar.getY(), is(0));
     }
+
+    @Test
+    public void constructOne() {
+        title("Construct one");
+        polar = new Polar(10, 10, 50, 30);
+        print(polar);
+        assertThat(polar.getLength(), is(50));
+        assertThat(polar.getAngle(), is(30));
+        assertThat(polar.getOrigin(), is(p(10, 10)));
+        assertThat(polar.getRect(), is(p(53, 34)));
+        assertThat(polar.getX(), is(53));
+        assertThat(polar.getY(), is(34));
+    }
+
+    @Test
+    public void copyConstruct() {
+        title("Copy Construct");
+        polar = new Polar(0, 0, 100, 180);
+        Polar copy = new Polar(polar);
+        print(copy);
+        assertThat(copy.getLength(), is(polar.getLength()));
+        assertThat(copy.getAngle(), is(polar.getAngle()));
+        assertThat(copy.getOrigin(), is(polar.getOrigin()));
+        assertThat(copy.getRect(), is(polar.getRect()));
+    }
+
+    @Test
+    public void constructFromCenterAndPointOne() {
+        title("Construct from center and point: 1");
+        polar = new Polar(p(-5, -5), p(25, 35));
+        print(polar);
+        assertThat(polar.getLength(), is(50));
+        assertThat(polar.getAngle(), is(53));
+    }
+
+    @Test
+    public void constructFromCenterAndPointZero() {
+        title("Construct from center and point: 0");
+        polar = new Polar(p(1, 2), p(1, 2));
+        print(polar);
+        assertThat(polar.getLength(), is(0));
+        assertThat(polar.getAngle(), is(0));
+    }
+
+    @Test
+    public void angleOverage() {
+        title("Angle overage");
+        polar = new Polar(0, 0, 100, 375);
+        print(polar);
+        assertThat(polar.getAngle(), is(15));
+    }
+
+    @Test
+    public void angleUnderage() {
+        title("Angle underage");
+        polar = new Polar(0, 0, 100, -340);
+        print(polar);
+        assertThat(polar.getAngle(), is(20));
+    }
+
+    // No unit tests for translate() -- deprecated
+    // No unit tests for midpoint() -- deprecated
+    // No unit tests for flip() -- deprecated
+
+    @Test
+    public void polarSetLength() {
+        title("polar set length");
+        polar = new Polar();
+        print(polar);
+        assertThat(polar.getLength(), is(0));
+        assertThat(polar.getRect(), is(p(0, 0)));
+
+        polar.setLength(12);
+        print(polar);
+        assertThat(polar.getLength(), is(12));
+        assertThat(polar.getRect(), is(p(12, 0)));
+    }
+
+    @Test
+    public void polarSetAngle() {
+        title("polar set angle");
+        polar = new Polar();
+        assertThat(polar.getAngle(), is(0));
+        polar.setAngle(90);
+        assertThat(polar.getAngle(), is(90));
+        polar.setAngle(370);
+        assertThat(polar.getAngle(), is(10));
+    }
+
+    @Test
+    public void polarAddAngle() {
+        title("polar add angle");
+        polar = new Polar();
+        assertThat(polar.getAngle(), is(0));
+        polar.addAngle(60);
+        assertThat(polar.getAngle(), is(60));
+        polar.addAngle(-20);
+        assertThat(polar.getAngle(), is(40));
+        polar.addAngle(180);
+        assertThat(polar.getAngle(), is(220));
+        polar.addAngle(155);
+        assertThat(polar.getAngle(), is(15));
+    }
+
+    // no unit tests for setOrigin(Point) -- deprecated
+    // no unit tests for setOrigin(int,int) -- deprecated
+
 }
